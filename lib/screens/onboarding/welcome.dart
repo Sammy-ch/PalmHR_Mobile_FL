23ed2a80c649ad20d573862356cd7a7436eda54f
Sammy-ch <sammystcherubin@gmail.com>
2024-06-15 09:00:39 +0200

    Minor WelcomeScreen UI update

--- lib/screens/onboarding/welcome.dart
+++ lib/screens/onboarding/welcome.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          const Text.rich(TextSpan(
-              text: 'Welcome to ',
-              style: TextStyle(
-                  color: Colors.teal, decoration: TextDecoration.none))),
-          ElevatedButton(
-              onPressed: () {
-                context.goNamed('/register');
-              },
-              child: const Text("Home Page"))
-        ],
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              colorFilter: ColorFilter.mode( Colors.black.withOpacity(0.5), BlendMode.darken),
+              image: AssetImage("assets/skyscraper2.jpg"),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                    width:double.infinity,
+                    height: 60.0,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: ElevatedButton(
+                            onPressed: (){
+                              context.goNamed("/login");
+                            },
+                            child: Text("Get Started",style: GoogleFonts.inter(fontSize: 20,fontWeight: FontWeight.w500)
+                            )
+                        )
+                    )
+                ),
+              )
+            ],
+          )
+        ),
       ),
     );
   }
