ac5d8c314668f59c2d9f65770937231e0b129a36
Sammy-ch <sammystcherubin@gmail.com>
2024-06-28 16:17:06 +0200

    Updated Login Screen UI

--- lib/screens/onboarding/login.dart
+++ lib/screens/onboarding/login.dart
@@ -1,11 +1,13 @@
 import 'package:PALMHR_MOBILE/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:modular_ui/modular_ui.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:gap/gap.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -14,31 +16,33 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-            Image.asset("assets/logo.png",scale: 15),
-            Gap(40),
-            Text("Log in to your \n Account",style: GoogleFonts.notoSans(fontSize: 30.0,color: Colors.white,fontWeight: FontWeight.bold)),
-              Text("Enter your Email and Password to login",style: GoogleFonts.notoSans(fontSize: 15.0,color: Colors.grey,fontWeight: FontWeight.w400)),
-              Gap(40),
-              loginForm()
-            ],),
-        ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/skyscraper.jpg",
+              fit: BoxFit.fill,
+            ),
+          ),
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Gap(15),
+                Image.asset("assets/logo.png", scale: 7),
+                Gap(80),
+                loginForm(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
 
 class loginForm extends StatefulWidget {
   const loginForm({super.key});
@@ -48,10 +52,9 @@ class loginForm extends StatefulWidget {
 }
 
 class _loginFormState extends State<loginForm> {
-
   late final TextEditingController _emailController = TextEditingController();
   late final TextEditingController _passwordController =
-  TextEditingController();
+      TextEditingController();
 
   bool _isLoading = false;
 
@@ -89,60 +92,130 @@ class _loginFormState extends State<loginForm> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration:  InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: BorderSide(color: Colors.white)
-                ),
-                labelText: "Email",
-                labelStyle: TextStyle(color: Colors.white),
-                filled: true,
-                fillColor: Colors.black,
-                  focusColor: Colors.green
-              ),
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(30.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(50.0),
+            topRight: Radius.circular(50.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10.0,
+              offset: Offset(0, 5),
             ),
-            const Gap(30),
-            TextField(
-              obscureText: true,
-              controller: _passwordController,
-              decoration:  InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: BorderSide(color: Colors.white)
-                  ),
-                  labelText: "Password",
-                  labelStyle: TextStyle(color: Colors.white),
-                  filled: true,
-                  fillColor: Colors.black,
-                  focusColor: Colors.green
-              ),
-            ),
-            Gap(50),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10.0),
-
-              child: SizedBox(
-                width: double.infinity,
-                  height: 60.0,
-                  child: ElevatedButton(
-                style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all(Colors.green.shade400)
-                ),
-                      onPressed: (){
-                  _signIn();
-                      }, child: Text("Log In",style: GoogleFonts.roboto(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),))),
-            )
           ],
-        )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Sign in to your account",
+              style: GoogleFonts.notoSans(
+                fontSize: 25.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Enter your Email and Password",
+              style: GoogleFonts.notoSans(
+                fontSize: 15.0,
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const Gap(40),
+            Form(
+                child: Column(
+              children: <Widget>[
+                TextField(
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                      icon: const FaIcon(
+                        Icons.mail_rounded,
+                        size: 30,
+                        color: Colors.green,
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 2,
+                            color: Colors.green,
+                            style: BorderStyle.solid),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide:
+                              const BorderSide(color: Colors.grey)),
+                      labelText: "Email",
+                      labelStyle: TextStyle(color: Colors.black),
+                      filled: true,
+                      fillColor: Colors.grey.shade100,
+                      ),
+                ),
+                const Gap(30),
+                TextField(
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  obscureText: true,
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      icon: const FaIcon(
+                        Icons.lock_outline,
+                        size: 30,
+                        color: Colors.green,
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(width: 2, color: Colors.green),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(color: Colors.grey)),
+                      labelText: "Password",
+                      labelStyle: TextStyle(color: Colors.black),
+                      filled: true,
+                      fillColor: Colors.grey.shade100,
+                      ),
+                ),
+                Gap(50),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: SizedBox(
+                      width: double.infinity,
+                      height: 60.0,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: WidgetStateProperty.all(
+                                  Colors.green.shade500)),
+                          onPressed: () {
+                            _signIn();
+                          },
+                          child: Text(
+                            "Login",
+                            style: GoogleFonts.roboto(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ))),
+                )
+              ],
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
